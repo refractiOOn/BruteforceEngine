af6de6d894653545fb9e038e1fc86b98a2a50ba9
refractiOOn <etozhesotnikow@gmail.com>
2023-05-04 03:10:51 +0300

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,8 @@
+from conan import ConanFile
+
+class Bruteforce(ConanFile):
+    name = 'Bruteforce'
+    version = '1.0'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    generators = 'CMakeToolchain', 'CMakeDeps'
+    requires = 'openssl/3.1.0', 'rapidjson/cci.20220822'
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "BruteforceEngine.hpp"
+
+int main(int, char**) {
+    //std::cout << "Hello, world!\n";
+}
